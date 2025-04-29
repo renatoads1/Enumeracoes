16b71fcc7991850d39e77c8b647ff9b6042d117c
renatoads1 <renato.lacerda>
2025-04-29 11:07:06 -0300

    Adicionar arquivos de projeto.

---
+++ Enumeracoes/Order.cs
@@ -0,0 +1,25 @@
+﻿using Enumeracoes.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeracoes
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return $"Order ID: {Id}, Product: {Product}, Quantity: {Quantity}, Price: {Price}, Status: {Status}";
+        }
+
+    }
+
+}
---
+++ Enumeracoes/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Enumeracoes;
+using Enumeracoes.Entities;
+
+Order order = new Order
+{
+    Id = 1,
+    Product = "Laptop",
+    Quantity = 2,
+    Price = 1500.00,
+    Status = OrderStatus.Processing
+};
+Console.WriteLine(order.ToString());
