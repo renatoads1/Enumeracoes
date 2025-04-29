7a85cdc891c55477643cbf2a8c72dd82f8333587
renatoads1 <renato.lacerda>
2025-04-29 15:50:40 -0300

    EXE 2

---
+++ Enumeracoes/Entities/Worker.cs
@@ -0,0 +1,53 @@
+﻿using Enumeracoes.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeracoes.Entities
+{
+    public class Worker
+    {
+        public string Name { get; set; }
+        public double BaseSalary { get; set; }
+        public WorkerLevel Level { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+        }
+
+        public Worker( string name, double baseSalary, WorkerLevel level, Department department)
+        {
+            
+            Name = name;
+            BaseSalary = baseSalary;
+            Level = level;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract) { 
+            Contracts.Remove(contract);
+        }
+
+        public double CalculateIncome(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts) {
+                if (year == contract.Date.Year && month == contract.Date.Month) { 
+                    sum += contract.TotalValue();
+                }
+            }
+            // Implementation for calculating income
+            return sum;
+        }
+    }
+
+}
--- Enumeracoes/Program.cs
+++ Enumeracoes/Program.cs
@@ -1,18 +1,60 @@
 ﻿using Enumeracoes.Entities;
+using System.Globalization;
 using Enumeracoes.Enum;
+//EXE2
+Console.WriteLine("Entre department name");
+string departmentName = Console.ReadLine();
+Console.WriteLine("Entre worker name");
+string workerName = Console.ReadLine();
+Console.WriteLine("Entre worker level");
+string workerLevel = Console.ReadLine();
+Console.WriteLine("Entre base salary");
+double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Quantos contratos tem esse trabalhador?");
+int n = int.Parse(Console.ReadLine());
+WorkerLevel level = Enum.Parse<WorkerLevel>(workerLevel);
 
-Order order = new Order
-{
-    Id = 1,
-    Product = "Laptop",
-    Quantity = 2,
-    Price = 1500.00,
-    Status = OrderStatus.Processing
-};
-Console.WriteLine(order.ToString());
+Department dep = new Department(departmentName);
+Worker worker = new Worker(workerName, baseSalary, level, dep);
+Console.WriteLine("hou many contracts for this worker? ");
+int contracts = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++) {
+    Console.WriteLine($"Enter {i} contract");
+    Console.WriteLine("Date:(dd/MM/yyyy)");
+    DateTime date = DateTime.Parse(Console.ReadLine());
+    Console.WriteLine("Value per hour");
+    double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    Console.WriteLine("Duration Hours");
+    int hours = int.Parse(Console.ReadLine());
+    HourContract contract = new HourContract(date, valuePerHour, hours);
+    worker.AddContract(contract);
+}
 
-string st = OrderStatus.PendingPayment.ToString();
-Console.WriteLine(st);
+Console.WriteLine("Entre year and month to calculate income (yyyy-MM)");
+string yearAndMonth = Console.ReadLine();
+int year = int.Parse(yearAndMonth.Substring(0,2));
+int month = int.Parse(yearAndMonth.Substring(3,4));
+double income = worker.CalculateIncome(year, month);
+Console.WriteLine("Name: " + worker.Name);
+Console.WriteLine("Department: " + worker.Department.Name);
+Console.WriteLine("Income for " + yearAndMonth + ": " + income.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Contracts: ");
+Console.WriteLine( income.ToString("F2", CultureInfo.InvariantCulture));
 
-OrderStatus os = Enum.Parse<OrderStatus>("Shipped");
-Console.WriteLine(os.ToString());
+
+//EXE1
+//Order order = new Order
+//{
+//    Id = 1,
+//    Product = "Laptop",
+//    Quantity = 2,
+//    Price = 1500.00,
+//    Status = OrderStatus.Processing
+//};
+//Console.WriteLine(order.ToString());
+
+//string st = OrderStatus.PendingPayment.ToString();
+//Console.WriteLine(st);
+
+//OrderStatus os = Enum.Parse<OrderStatus>("Shipped");
+//Console.WriteLine(os.ToString());
